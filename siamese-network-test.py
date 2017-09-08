4b9d84d26565af5d2d7c3ba532f4aea87d569902
Ujjwal <ujjwalupadhyay8@gmail.com>
2017-09-08 23:50:34 +0530

    Test Siamese Network

---
+++ siamese-network-test.py
@@ -0,0 +1,18 @@
+folder_dataset_test = dset.ImageFolder(root=Config.testing_dir)
+siamese_dataset = SiameseNetworkDataset(imageFolderDataset=folder_dataset_test,
+                                        transform=transforms.Compose([transforms.Scale((100,100)),
+                                                                      transforms.ToTensor()
+                                                                      ])
+                                       ,should_invert=False)
+
+test_dataloader = DataLoader(siamese_dataset,num_workers=6,batch_size=1,shuffle=True)
+dataiter = iter(test_dataloader)
+x0,_,_ = next(dataiter)
+
+for i in range(10):
+    _,x1,label2 = next(dataiter)
+    concatenated = torch.cat((x0,x1),0)
+    
+    output1,output2 = net(Variable(x0).cuda(),Variable(x1).cuda())
+    euclidean_distance = F.pairwise_distance(output1, output2)
+    imshow(torchvision.utils.make_grid(concatenated),'Dissimilarity: {:.2f}'.format(euclidean_distance.cpu().data.numpy()[0][0]))
